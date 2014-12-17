92e88c4382fc1dda783885dd1fa71fb5bc1049ad
Dane Springmeyer <dane@mapbox.com>
2014-12-16 21:10:54 -0800

    no need for rx/ry

--- src/symbolizer_keys.cpp
+++ src/symbolizer_keys.cpp
@@ -158,9 +158,7 @@ static const property_meta_type key_meta[const_max_key] =
                         {return enumeration<text_upright_enum,text_upright_enum_MAX>(text_upright_enum(e.value)).as_string();},
                         property_types::target_upright},
     property_meta_type{ "avoid-edges",nullptr, property_types::target_bool },
-    property_meta_type{ "font-feature-settings", nullptr, property_types::target_font_feature_settings },
-    property_meta_type{ "rx", nullptr, property_types::target_double },
-    property_meta_type{ "ry", nullptr, property_types::target_double }
+    property_meta_type{ "font-feature-settings", nullptr, property_types::target_font_feature_settings }
 };
 
 property_meta_type const& get_meta(mapnik::keys key)
